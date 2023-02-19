b5c833feadc585c9351e71e6d6d16c7de126493a
1 <1@LAPTOP-8U2CR0NO>
2023-02-19 19:17:06 +0300

    Добавьте файлы проекта.

---
+++ wek17/wek17.cpp
@@ -0,0 +1,55 @@
+﻿
+
+#include <iostream>
+using namespace std;
+//задание 1 и 2, добавление столбца в двумерный массив, и удаление
+void addarray(int** arr, int row, int colum, int index)//+
+{
+    
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colum; j++)
+        {
+            
+            if (j == index)
+            {
+                for (int k = colum; k > index; k--)
+                {
+                    arr[i][k] = arr[i][k - 1];
+                }
+               
+                arr[i][j] = 0;
+            }
+        }
+    }
+}
+void deletearrey(int** arr, int row, int colum, int index)//-
+{
+    
+    for (int i = 0; i < row; i++)
+    {
+        
+        for (int j = index; j < colum; j++)
+        {
+            arr[i][j] = arr[i][j + 1];
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+}
+
